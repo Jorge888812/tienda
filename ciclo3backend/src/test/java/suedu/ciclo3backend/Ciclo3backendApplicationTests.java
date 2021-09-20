9e98b7c971b4c185ca941b1d560ff376a07acfd2
Jorge888812 <90994673+Jorge888812@users.noreply.github.com>
2021-09-20 14:45:53 -0500

    primera edition

---
+++ ciclo3backend/src/test/java/suedu/ciclo3backend/Ciclo3backendApplicationTests.java
@@ -0,0 +1,13 @@
+package suedu.ciclo3backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ciclo3backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
